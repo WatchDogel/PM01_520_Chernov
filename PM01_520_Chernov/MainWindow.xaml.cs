086c4cce714eda6fab831e4a2d5de50e5aebc7cd
208492 <208492@KB37-1-120-C05>
2024-04-15 17:20:44 +0300

    Добавьте файлы проекта.

---
+++ PM01_520_Chernov/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PM01_520_Chernov
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            var exit = MessageBox.Show("Вы действительно хотите закрыть приложение?", "Выход из приложения", MessageBoxButton.YesNo);
+            if (exit == MessageBoxResult.Yes)
+                Application.Current.Shutdown();
+            else
+                e.Cancel = true;
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            if (MainFrame.CanGoBack) MainFrame.GoBack();
+        }
+    }
+}
+
---
+++ PM01_520_Chernov/Pages/AuthPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PM01_520_Chernov.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthPage.xaml
+    /// </summary>
+    public partial class AuthPage : Page
+    {
+        public AuthPage()
+        {
+            InitializeComponent();
+        }
+        private void login_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            {
+                txtHintLogin.Visibility = Visibility.Visible;
+                if (login.Text.Length > 0)
+                {
+                    txtHintLogin.Visibility = Visibility.Hidden;
+                }
+            }
+
+        }
+        private void PassWr_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            {
+                PassHint.Visibility = Visibility.Visible;
+                if (PassWr.Password.Length > 0)
+                {
+                    PassHint.Visibility = Visibility.Hidden;
+                }
+            }
+
+        }
+
+        private void vhod_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(login.Text) || string.IsNullOrEmpty(PassWr.Password))
+            {
+                MessageBox.Show("Введите логин и пароль!");
+                return;
+            }
+        }
+    }
+}
